a8abaeaa8e8d357d3bcd80e6fb55259e47bacdcb
Matt Moore <matt@mattmoore.io>
2020-03-15 00:32:00 -0500

    Add more hashing functions and check function.

--- src/main/scala/io/mattmoore/scala/bcrypt/BCrypt.scala
+++ src/main/scala/io/mattmoore/scala/bcrypt/BCrypt.scala
@@ -7,4 +7,25 @@ object BCrypt {
     } catch {
       case e: Exception => Left(e.getMessage)
     }
+
+  def hashpw(plaintext: String, logRounds: Int): Either[String, String] =
+    try {
+      Right(org.mindrot.jbcrypt.BCrypt.hashpw(plaintext, org.mindrot.jbcrypt.BCrypt.gensalt(logRounds)))
+    } catch {
+      case e: Exception => Left(e.getMessage)
+    }
+
+  def hashpw(plaintext: String): Either[String, String] =
+    try {
+      Right(org.mindrot.jbcrypt.BCrypt.hashpw(plaintext, org.mindrot.jbcrypt.BCrypt.gensalt))
+    } catch {
+      case e: Exception => Left(e.getMessage)
+    }
+
+  def checkpw(plaintext: String, hashed: String): Either[String, Boolean] =
+    try {
+      Right(org.mindrot.jbcrypt.BCrypt.checkpw(plaintext, hashed))
+    } catch {
+      case e: Exception => Left(e.getMessage)
+    }
 }
--- src/test/scala/io/mattmoore/scala/bcrypt/BCryptSpec.scala
+++ src/test/scala/io/mattmoore/scala/bcrypt/BCryptSpec.scala
@@ -3,9 +3,27 @@ package io.mattmoore.scala.bcrypt
 import org.scalatest._
 
 class BCryptSpec extends FlatSpec with Matchers {
-  "hashpw" should "hash the password with a generated salt" in {
-    val salt = "$2a$10$GgITn8IWUCHRzar0BnjgMO"
-    val password = "password"
-    BCrypt.hashpw(password, salt) shouldEqual Right("$2a$10$GgITn8IWUCHRzar0BnjgMODAGSDM.Io26kJga0L5PVU0JQd4AHB96")
+  val password = "password"
+  val salt = "$2a$10$GgITn8IWUCHRzar0BnjgMO"
+  val hashed = "$2a$10$GgITn8IWUCHRzar0BnjgMODAGSDM.Io26kJga0L5PVU0JQd4AHB96"
+
+  "hashpw" should "hash the password with a provided salt" in {
+    BCrypt.hashpw(password, salt) shouldEqual Right(hashed)
+  }
+
+  "hashpw" should "hash the password with an auto-generated salt with specified rounds" in {
+    BCrypt.hashpw(password, 12).isRight shouldBe true
+  }
+
+  "hashpw" should "hash the password with an auto-generated salt with default rounds" in {
+    BCrypt.hashpw(password).isRight shouldBe true
+  }
+
+  "checkpw" should "return true if the password is valid" in {
+    BCrypt.checkpw(password, hashed) shouldBe Right(true)
+  }
+
+  "checkpw" should "return false if the password is invalid" in {
+    BCrypt.checkpw("wrong", hashed) shouldBe Right(false)
   }
 }
