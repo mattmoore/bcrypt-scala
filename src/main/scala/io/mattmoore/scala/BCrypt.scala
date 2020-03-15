746f75e11b1b18178f1ae26cde27222de495b716
Matt Moore <matt@mattmoore.io>
2020-03-14 22:07:27 -0500

    Wrap jbcrypt.

--- src/main/scala/io/mattmoore/scala/BCrypt.scala
+++ src/main/scala/io/mattmoore/scala/BCrypt.scala
@@ -1,6 +1,10 @@
-package io.mattmoore.scala
+package io.mattmoore.scala.bcrypt
 
 object BCrypt {
-  def hashpw(plaintext: String): Either[Exception, String] =
-    Right(plaintext)
+  def hashpw(plaintext: String, salt: String): Either[String, String] =
+    try {
+      Right(org.mindrot.jbcrypt.BCrypt.hashpw(plaintext, salt))
+    } catch {
+      case e: Exception => Left(e.getMessage)
+    }
 }
--- src/test/scala/io/mattmoore/scala/BCryptSpec.scala
+++ src/test/scala/io/mattmoore/scala/BCryptSpec.scala
@@ -1,9 +1,11 @@
-package io.mattmoore.scala
+package io.mattmoore.scala.bcrypt
 
 import org.scalatest._
 
 class BCryptSpec extends FlatSpec with Matchers {
   "hashpw" should "hash the password with a generated salt" in {
-    BCrypt.hashpw("hello") shouldEqual Right("hello")
+    val salt = "$2a$10$GgITn8IWUCHRzar0BnjgMO"
+    val password = "password"
+    BCrypt.hashpw(password, salt) shouldEqual Right("$2a$10$GgITn8IWUCHRzar0BnjgMODAGSDM.Io26kJga0L5PVU0JQd4AHB96")
   }
 }
